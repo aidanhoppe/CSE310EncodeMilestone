69e63b0773cb48987a87d9534cdfc61263009c84
aidanhoppe <68791485+aidanhoppe@users.noreply.github.com>
2020-10-01 23:37:31 -0700

    Add files via upload
    final

--- decoderProject1.cpp
+++ decoderProject1.cpp
@@ -9,9 +9,7 @@ Description: This is the encoding portion of assignment 1. It creates string per
 #include <iostream>
 #include <cstdlib>
 #include <string>
-//#include <chrono>
 using namespace std;
-//using namespace std::chrono;
 
 char* arrayFromLine();
 char* sortArray(char* array, int size);
@@ -24,7 +22,6 @@ int partition(char* sortedColumn, int low, int high, int length);
 
 int main(int argc, char** argv) {
 	
-//		auto start = high_resolution_clock::now();
 	
 		string toBeDecoded;
 		int stringLength;
@@ -43,11 +40,11 @@ int main(int argc, char** argv) {
 			cout << "Sorry, only quick and insertion sort are supported" << endl;
 			return 0;
 		}
-		while(getline(cin, toBeDecoded)){
+		while(getline(cin, toBeDecoded)){ //main loop continues while there are more lines to read
 			
 			stringLength = toBeDecoded.length();
 			
-			while(stringLength == 0){
+			while(stringLength == 0){ //skip empty lines and replace them with empty lines. search for index
 				cout << endl;
 				getline(cin, toBeDecoded);
 				stringLength = toBeDecoded.length();
@@ -55,76 +52,56 @@ int main(int argc, char** argv) {
 			
 			tempColumn = new char[1000];
 			
-			index = atoi(toBeDecoded.c_str());
-			
-			//cout << index << endl;
-			
-			getline(cin, toBeDecoded);
+			index = atoi(toBeDecoded.c_str());  //parse the index and store as an integer
+						
+			getline(cin, toBeDecoded); //get the line to be decoded after you parse integer
 			stringLength = toBeDecoded.length();
 			
-			//char* array = arrayFromLine(toBeDecoded);
 			
-			int loopLength = stringLength/4+1;
+			int loopLength = stringLength/4+1; //create loop length by identifying how many clusters are in the string
 			stringLength = 0;
 			repetitions = 0;
 			for(int i = 0; i<loopLength; i++){
 				repetitions = toBeDecoded[4*i]-'0';
 				for(int j = 0; j<repetitions; j++){
-					tempColumn[stringLength + j] = toBeDecoded[2+4*i];
-					//cout << toBeDecoded[2+4*i];
+					tempColumn[stringLength + j] = toBeDecoded[2+4*i]; //add copies of char values to tempColumn representative of how many times it is repeated
 				}
-				stringLength += repetitions;
+				stringLength += repetitions; //iterate string length by repetitions to always print on new indices
 			}
 			
 
+			//dynamically declare the important structures for this algorithm based on current string length
 			lastColumn = new char[stringLength];
 			sortedColumn = new char[stringLength];
 			checkedArray = new int[stringLength];
-//			for(int i = 0; i<stringLength; i++){
-//				checkedArray[i]=0;
-//			}
 			nextArray = new int[stringLength];
 			
-			for(int i = 0; i<stringLength; i++){
+			for(int i = 0; i<stringLength; i++){ //copy tempColumn to last and sorted. sorted will then be sorted
 				lastColumn[i] = tempColumn[i];
 				sortedColumn[i] = tempColumn[i];
 			}
 			delete[] tempColumn;
 			
-			if(sortType == "insertion"){
+			if(sortType == "insertion"){ //sort the unsorted 'sortedColumn' using either insertion or quicksort
 				sortColumnInsertion(sortedColumn, stringLength);
 			} else {
 				sortColumnQuick(sortedColumn, 0 , stringLength-1, stringLength);
 			}
 			
-			fillNext(lastColumn, sortedColumn, nextArray, stringLength, checkedArray);
+			fillNext(lastColumn, sortedColumn, nextArray, stringLength, checkedArray); //create the 'next' array from last column and sorted column
 			
-			outputFromNext(nextArray, sortedColumn, stringLength, index);
+			outputFromNext(nextArray, sortedColumn, stringLength, index); //print out the final strings from nextArray and sortedColumn
 			
-			delete[] lastColumn;
+			delete[] lastColumn; //clean up before looping again
 			delete[] sortedColumn;
-		//	delete[] checkedArray;
-		//	delete[] nextArray;
-			
-//			lastColumn = nullptr;
-//			sortedColumn = nullptr;
-//			checkedArray = nullptr;
-//			nextArray = nullptr;
 		}
-		
-//		auto stop = high_resolution_clock::now();
-//		
-//		auto duration = duration_cast<microseconds>(stop-start);
-//		
-//		cout << "time: ";
-//		cout << duration.count();
 		
 		return 1;
 		
 }
 
-void fillNext(char* lastColumn, char* sortedColumn, int* nextArray, int length, int* checkedArray){
-	for(int i = 0; i<length; i++){
+void fillNext(char* lastColumn, char* sortedColumn, int* nextArray, int length, int* checkedArray){ //fillNext will create the integer array nextAray by utilizing checkedArray
+	for(int i = 0; i<length; i++){                                                                  	//and the sorted and last columns.
 		for(int j = 0; j<length; j++){
 			if(sortedColumn[i] == lastColumn[j] && checkedArray[j]!=1){
 				nextArray[i]=j;
@@ -135,7 +112,7 @@ void fillNext(char* lastColumn, char* sortedColumn, int* nextArray, int length, 
 	}
 }
 
-void outputFromNext(int* nextArray, char* sortedColumn, int stringLength, int index){
+void outputFromNext(int* nextArray, char* sortedColumn, int stringLength, int index){ //create string output from nextArray and sortedColumn
 	for(int i = 0; i<stringLength; i++){
 		cout << sortedColumn[index];
 		index = nextArray[index];
@@ -143,7 +120,7 @@ void outputFromNext(int* nextArray, char* sortedColumn, int stringLength, int in
 	cout<<endl;
 }
 
-void sortColumnInsertion(char* sortedColumn, int stringLength){
+void sortColumnInsertion(char* sortedColumn, int stringLength){ //insertion sort for array of chars
 	int i, j;
 	char key;
 	for(j =1; j<stringLength; j++){ //this is the insertion sort implementation
@@ -157,7 +134,7 @@ void sortColumnInsertion(char* sortedColumn, int stringLength){
 	}
 }
 
-void sortColumnQuick(char* sortedColumn, int low, int high, int length){
+void sortColumnQuick(char* sortedColumn, int low, int high, int length){ //quicksort body -- relies on partition
 	if(low<high){
 		int partitionIndex = partition(sortedColumn, low, high, length);
 		sortColumnQuick(sortedColumn, low, partitionIndex-1, length);
@@ -165,14 +142,14 @@ void sortColumnQuick(char* sortedColumn, int low, int high, int length){
 	}
 }
 
-int partition(char* sortedColumn, int low, int high, int length){
+int partition(char* sortedColumn, int low, int high, int length){ //most of the heavy lifting of quicksort. chooses partition element and swaps around it
 	char pivot = sortedColumn[high];
 	int i = (low-1);
 	
 	for(int j = low; j <= high-1; j++){
 		if(pivot > sortedColumn[j]){
 			i++;
-			char temp = sortedColumn[i];
+			char temp = sortedColumn[i]; //swap around partition element if needed
 			sortedColumn[i] = sortedColumn[j];
 			sortedColumn[j] = temp;
 		}
